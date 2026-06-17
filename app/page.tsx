6c1b0c63bfae27a836e00c4947df4800ca43ab79
Abdul221221-tech <abdulwaheed723485>
2026-06-17 22:08:37 +0530

    Changes made

--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,7 @@ import Chatbot from "@/components/Chatbot";
 import FloatingIcons from "@/components/FloatingIcons";
 
 export default function Home() {
-  const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(true)
 
   return (
     <>
